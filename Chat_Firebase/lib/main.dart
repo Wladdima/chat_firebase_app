3a601b5a4bf19fae56fb44e94c4b7e405a3bfd69
Wladdima <60410078+Wladdima@users.noreply.github.com>
2022-04-07 13:58:02 +0600

    chat_app

---
+++ Chat_Firebase/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_firebase/screens/welcome_screen.dart';
+import 'package:chat_firebase/screens/login_screen.dart';
+import 'package:chat_firebase/screens/registration_screen.dart';
+import 'package:chat_firebase/screens/chat_screen.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id.toString(): (context) => WelcomeScreen(),
+        LoginScreen.id.toString(): (context) => LoginScreen(),
+        RegistrationScreen.id.toString(): (context) => RegistrationScreen(),
+        ChatScreen.id.toString(): (context) => ChatScreen(),
+      },
+    );
+  }
+}
---
+++ Chat_Firebase/lib/screens/welcome_screen.dart
@@ -0,0 +1,105 @@
+import 'package:chat_firebase/screens/login_screen.dart';
+import 'package:chat_firebase/screens/registration_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:chat_firebase/components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      child: Image.asset('images/logo.png'),
+                      height: 60.0,
+                    ),
+                  ),
+                ),
+                AnimatedTextKit(
+                  repeatForever: true,
+                  pause: const Duration(seconds: 5),
+                  animatedTexts: [
+                    TyperAnimatedText(
+                      'Salam Chat',
+                      speed: Duration(milliseconds: 200),
+                      textStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              text: 'Войти',
+              onPressed: (){
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            RoundedButton(
+              color: Colors.blueAccent,
+              text: 'Регистрация',
+              onPressed: (){
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
